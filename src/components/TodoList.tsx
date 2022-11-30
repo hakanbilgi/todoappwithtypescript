fdf502c55f45845b03bfe973e3b7b6f4fa6703c3
Hakan Bilgi <hknblg34@gmail.com>
2022-11-30 20:49:58 +0300

    first commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TodoListItem from './TodoListItem'
+
+
+interface ITodoList {
+  todos:TodoType[]
+}
+
+const TodoList:React.FC<ITodoList>= ({todos}) => {
+  return (
+    <ul>
+      <TodoListItem />
+    </ul>
+  )
+}
+
+export default TodoList
---
+++ src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import InputForm from '../components/InputForm'
+import TodoList from '../components/TodoList';
+
+const url = "https://6352477ea9f3f34c3739206a.mockapi.io/todos";
+
+
+const Home = () => {
+  const [todos, setTodos] = useState<TodoType[]>([])
+
+  const getTodos = async()=> {
+    try {
+      const { data } = await axios.get(url);
+      setTodos(data)
+      console.log(data);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // useEffect(() => {
+  //   getTodos()
+  // }, [])
+  
+  return (
+    <div className='main'>
+      <InputForm  />
+      <TodoList todos={todos} />
+    </div>
+  )
+}
+
+export default Home
